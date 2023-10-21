c96bcb82963915dcb921b06bc80fdd87cf93bead
Harsh <57228178+harsh3294@users.noreply.github.com>
2023-10-21 19:39:20 +0530

    Completed working with landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import ClientProvider from "@/components/providers/ClientProvider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SaaS-ChatApplication",
@@ -20,7 +17,7 @@ export default function RootLayout({
   return (
     <ClientProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className="flex flex-col min-h-screen">
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
