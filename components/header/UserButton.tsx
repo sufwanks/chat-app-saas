2dd6d46925d0cad6c3824966f2c789ebf58badba
Harsh <57228178+harsh3294@users.noreply.github.com>
2023-11-11 11:02:18 +0530

    completed with the manage billing and create the stripe session checkout to manage the membership

--- components/header/UserButton.tsx
+++ components/header/UserButton.tsx
@@ -10,7 +10,12 @@ import {
 import UserAvatar from "./UserAvatar";
 import { Button } from "../ui/button";
 import { signIn, signOut } from "next-auth/react";
+import LoadingSpinner from "../loaders/LoadingSpinner";
+import { useSubscriptionStore } from "@/store/store";
+import { StarIcon } from "lucide-react";
+import ManageAccountButton from "../account/ManageAccountButton";
 function UserButton({ session }: IUserSession) {
+  const subscription = useSubscriptionStore((state) => state.subscription);
   //Session...
   if (!session)
     return (
@@ -27,6 +32,23 @@ function UserButton({ session }: IUserSession) {
         <DropdownMenuContent>
           <DropdownMenuLabel>{session.user?.name}</DropdownMenuLabel>
           <DropdownMenuSeparator />
+          {subscription === undefined && (
+            <DropdownMenuItem>
+              <LoadingSpinner />
+            </DropdownMenuItem>
+          )}
+          {subscription?.role === "pro" && (
+            <>
+              <DropdownMenuLabel className="text-xs flex items-center justify-center space-x-1 text-[#E935C1] animate-pulse">
+                <StarIcon fill="#E935C1" />
+                <p>PRO</p>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <ManageAccountButton />
+              </DropdownMenuItem>
+            </>
+          )}
           <DropdownMenuItem onClick={() => signOut()}>
             Sign Out
           </DropdownMenuItem>
--- components/pricing/CheckoutButton.tsx
+++ components/pricing/CheckoutButton.tsx
@@ -5,10 +5,18 @@ import { addDoc, collection, onSnapshot } from "firebase/firestore";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 import LoadingSpinner from "../loaders/LoadingSpinner";
+import { useSubscriptionStore } from "@/store/store";
+import ManageAccountButton from "../account/ManageAccountButton";
 
 function CheckoutButton() {
   const { data: session } = useSession();
   const [loading, setLoading] = useState(false);
+  const subscription = useSubscriptionStore((state) => state.subscription);
+
+  const isLoadingSubscription = subscription === undefined;
+  const isSubscribed =
+    subscription?.status === "active" && subscription?.role === "pro";
+
   const createCheckoutSession = async () => {
     if (!session?.user.id) return;
 
@@ -48,12 +56,15 @@ function CheckoutButton() {
   return (
     <div className="flex flex-col space-y-2">
       {/* If subscribed show me the user is subscribed */}
-      <button
-        onClick={() => createCheckoutSession()}
-        className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white   disabled:cursor-default"
-      >
-        {loading ? <LoadingSpinner /> : "Sign Up"}
-      </button>
+      <div className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer disabled:opacity-80 disabled:bg-indigo-600/50 disabled:text-white   disabled:cursor-default">
+        {isSubscribed ? (
+          <ManageAccountButton />
+        ) : isLoadingSubscription || loading ? (
+          <LoadingSpinner />
+        ) : (
+          <button onClick={() => createCheckoutSession()}>Sign Up</button>
+        )}
+      </div>
     </div>
   );
 }
