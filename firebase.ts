ba5f5c6814a263b68dc0b2a4a512fb5a70622657
Harsh <57228178+harsh3294@users.noreply.github.com>
2023-11-18 21:50:37 +0530

    changed the config and provided the env file

--- firebase.ts
+++ firebase.ts
@@ -3,23 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getFunctions } from "firebase/functions";
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY!,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN!,
-//   projectId: process.env.FIREBASE_PROJECT_ID!,
-//   storageBucket: process.env.FRIEBASE_STORAGE_BUCKET!,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID!,
-//   appId: process.env.FIREBASE_APP_ID!,
-//   measurementId: process.env.FIREBASE_MEASUREMENT_ID!,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyC7Ju4HZ9moCd-f3LmnnrNxvwzmscI5eCE",
-  authDomain: "saas-chatapplication.firebaseapp.com",
-  projectId: "saas-chatapplication",
-  storageBucket: "saas-chatapplication.appspot.com",
-  messagingSenderId: "514604485669",
-  appId: "1:514604485669:web:553aafced2eb45c32e83cc",
-  measurementId: "G-KFMBL1HQLE",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FRIEBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!,
 };
 
 // Initialize Firebase
